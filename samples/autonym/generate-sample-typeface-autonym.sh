65bf340a75354e3b4a360f5e819617b35108ee7b
Samyak Bhuta <samyak.bhuta@gmail.com>
2013-12-03 17:29:49 +0530

    Script to generate sample typeface autonyms. #13

---
+++ samples/autonym/generate-sample-typeface-autonym.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo "આકાર" | \convert -background none -pointsize 10 -font ../../aakar-medium.ttf text: -trim AakarSampleText_Aakar_10.png
+echo "આકાર" | \convert -background none -pointsize 50 -font ../../aakar-medium.ttf text: -trim AakarSampleText_Aakar_50.png
+echo "આકાર" | \convert -background none -pointsize 100 -font ../../aakar-medium.ttf text: -trim AakarSampleText_Aakar_100.png
