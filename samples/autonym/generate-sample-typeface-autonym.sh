2692624fd2dc6645c43f4242b8c0f7f522e1c933
Samyak Bhuta <samyak.bhuta@gmail.com>
2013-12-03 18:06:28 +0530

    Refactor.

--- samples/autonym/generate-sample-typeface-autonym.sh
+++ samples/autonym/generate-sample-typeface-autonym.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-echo "આકાર" | \convert -background none -pointsize 10 -font ../../aakar-medium.ttf text: -trim AakarSampleText_Aakar_10.png
-echo "આકાર" | \convert -background none -pointsize 50 -font ../../aakar-medium.ttf text: -trim AakarSampleText_Aakar_50.png
-echo "આકાર" | \convert -background none -pointsize 100 -font ../../aakar-medium.ttf text: -trim AakarSampleText_Aakar_100.png
+sampleText="આકાર"
+echo $sampleText | \convert -background none -pointsize 10 -font ../../aakar-medium.ttf text: -trim AakarSampleText_Aakar_10.png
+echo $sampleText | \convert -background none -pointsize 50 -font ../../aakar-medium.ttf text: -trim AakarSampleText_Aakar_50.png
+echo $sampleText | \convert -background none -pointsize 100 -font ../../aakar-medium.ttf text: -trim AakarSampleText_Aakar_100.png
