e58b363f41fd51d26f9df4c2b0f04656c032994a
Samyak Bhuta <samyak.bhuta@gmail.com>
2013-12-03 17:40:25 +0530

    Script to generate sample 'khalasaji' text. #13

---
+++ samples/khalasaji/generate-sample-khalasaji.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "ઇ.સ. 1978 ની 25 તારીખે, 06-34 વાગે, ઐશ્વર્યવાન, વફાદાર , અંગ્રેજ ઘરધણીના આ ઝાડ પાસે ઉભેલા બાદશાહ; અને ઓસરીમાંના ઠળીયા તથા છાણાના ઢગલા દુર કરીને, ઔપચારીકતાથી ઉભેલા ઋષી સમાન પ્રજ્ઞાચક્ષુ ખાલસાજી ભટ મળ્યા હતા.
+" | \convert -background none -pointsize 10 -font ../../aakar-medium.ttf text: -trim AakarSampleText_Khalasaji_10.png
+echo "ઇ.સ. 1978 ની 25 તારીખે, 06-34 વાગે, ઐશ્વર્યવાન, વફાદાર , અંગ્રેજ ઘરધણીના આ ઝાડ પાસે ઉભેલા બાદશાહ; અને ઓસરીમાંના ઠળીયા તથા છાણાના ઢગલા દુર કરીને, ઔપચારીકતાથી ઉભેલા ઋષી સમાન પ્રજ્ઞાચક્ષુ ખાલસાજી ભટ મળ્યા હતા.
+" | \convert -background none -pointsize 50 -font ../../aakar-medium.ttf text: -trim AakarSampleText_Khalasaji_50.png
+echo "ઇ.સ. 1978 ની 25 તારીખે, 06-34 વાગે, ઐશ્વર્યવાન, વફાદાર , અંગ્રેજ ઘરધણીના આ ઝાડ પાસે ઉભેલા બાદશાહ; અને ઓસરીમાંના ઠળીયા તથા છાણાના ઢગલા દુર કરીને, ઔપચારીકતાથી ઉભેલા ઋષી સમાન પ્રજ્ઞાચક્ષુ ખાલસાજી ભટ મળ્યા હતા.
+" | \convert -background none -pointsize 100 -font ../../aakar-medium.ttf text: -trim AakarSampleText_Khalasaji_100.png
